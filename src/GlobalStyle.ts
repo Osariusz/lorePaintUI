19ce979d7fc4b9b0fcc97378aa7f86eb10e20d28
Osariusz <osariusz@interia.pl>
2024-03-08 20:51:26 +0100

    Place optional arguments

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -8,6 +8,29 @@ const GlobalStyle = createGlobalStyle`
     background: #F5F5DC;
     color: #B22222;
     font-family: Open-Sans, Helvetica, Sans-Serif;
+      height: inherit;
+      width: inherit;
+  }
+  html {
+      position: absolute;
+      height: 100%;
+      width: 100%;
+  }
+  #root {
+      width: inherit;
+      height: inherit;
+  }
+  div {
+      width: inherit;
+      height: inherit;
+  }
+  svg {
+      width: inherit;
+      height: inherit;
+  }
+  g {
+      width: inherit;
+      height: inherit;
   }
 `;
 
--- src/place/Place.tsx
+++ src/place/Place.tsx
@@ -2,18 +2,26 @@ import React from "react";
 import {Marker, Point} from "react-simple-maps";
 
 interface PlaceProps {
-    link: string;
-    size: Point;
+    link?: string;
+    size?: Point;
     position: Point;
 }
 
-const Place: React.FC<PlaceProps> = ({link, size, position}) => {
-    return (
-        <Marker coordinates={position}>
-            <image style={{width: size.at(0), height: size.at(1)}}
-                   href={link}/>
-        </Marker>
+const defaultProps = {
+    link: "default",
+    size: [50,50] as Point
+}
 
+const Place = (props: PlaceProps) => {
+    props = {...defaultProps, ...props}
+    if(props.link === "default") {
+        props.link = "https://cdn.discordapp.com/attachments/436214161077436426/1215734565340184636/tempplacelogo.png?ex=65fdd40b&is=65eb5f0b&hm=c12b17b57cf5f6c2fd0f2e64f46daad60b5462efb0b265e3f327c9c6c1d32a2f&"
+    }
+     return (
+        <Marker coordinates={props.position}>
+            <image style={{width: props.size?.at(0), height: props.size?.at(1)}}
+                   href={props.link}/>
+        </Marker>
     );
 };
 
