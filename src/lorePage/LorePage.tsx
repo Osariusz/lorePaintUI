9f24f805987261cf9421424857c06511980a3ce2
Osariusz <osariusz@interia.pl>
2024-03-09 16:12:09 +0100

    Map center calculation

--- src/lorePage/LorePage.tsx
+++ src/lorePage/LorePage.tsx
@@ -8,16 +8,36 @@ import Place from "../place/Place";
 const LorePage = () => {
     const [places, setPlaces] = useState([]);
 
+    const imageWidth = 1653;
+    const imageHeight = 796;
+
     // @ts-ignore
     const PlaceWithMapContext = ({ position }) => {
         const { projection } = useMapContext();
         const projectedPosition = projection(position);
+        console.log("map position")
         console.log(projectedPosition);
         return <Place position={projectedPosition} />;
     };
 
+
+
     const clickHandler = (event:any) => {
-        const coordinates = [event.clientX, event.clientY];
+        let {innerWidth: width, innerHeight: height} = window;
+
+        let aspectRatio = width/height;
+
+        if(imageWidth/imageHeight > aspectRatio) {
+            height = (imageHeight/imageWidth)*width;
+        }
+        else {
+            width = (imageWidth/imageHeight)*height;
+        }
+
+
+        const coordinates = [event.clientX-width/2, event.clientY-height/2];
+        console.log("click coords")
+        console.log(coordinates)
         addPlace(coordinates);
     }
 
@@ -31,7 +51,6 @@ const LorePage = () => {
             const { projection } = useMapContext()
             const [x1, y1] = projection(coordinates[0])
             const [x2, y2] = projection(coordinates[1])
-            console.log([x1,y1])
             return <line x1={x1} y1={y1} x2={x2} y2={y2} {...restProps} />
         }
     )
@@ -42,7 +61,7 @@ const LorePage = () => {
     }
 
     return (
-        <ComposableMap width={1653} height={796} projection="geoMercator" projectionConfig={{center: [0,0]}} onClick={clickHandler}>
+        <ComposableMap width={imageWidth} height={imageHeight} projection="geoMercator" projectionConfig={{center: [0,0]}} onClick={clickHandler}>
             <ZoomableGroup zoom={1} maxZoom={20} minZoom={0} style={{border:5, borderColor: "black"}}>
                 <image id={"image"} href={"https://cdn.discordapp.com/attachments/436214161077436426/1215728818174697513/image.png?ex=65fdceb1&is=65eb59b1&hm=b43de10d49dc3ef3bc227ff0c541b10642d10d83accc15ff85e46c3ff016f506&"}/>
                 {places}
