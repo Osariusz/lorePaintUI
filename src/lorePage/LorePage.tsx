fc1a69a41e2d6cb09bedd9f9c965f7989b09fd33
Osariusz <osariusz@interia.pl>
2024-03-11 00:53:39 +0100

    Open layer init

--- src/lorePage/LorePage.tsx
+++ src/lorePage/LorePage.tsx
@@ -1,32 +1,77 @@
-import React, {ForwardedRef, forwardRef, useEffect, useState} from "react";
-// @ts-ignore
-import {ComposableMap, ZoomableGroup, useZoomPanContext, useMapContext, Point} from "react-simple-maps";
+import React, {useEffect, useRef, useState} from "react";
+import Map from "ol/Map";
+import View from "ol/View";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import {fromLonLat, Projection} from "ol/proj";
+import Feature from "ol/Feature";
+import Point from "ol/geom/Point";
+import VectorSource from "ol/source/Vector";
+import VectorLayer from "ol/layer/Vector";
 import Place from "../place/Place";
+import {Icon, Style} from "ol/style";
+import ImageLayer from "ol/layer/Image";
+import Static from "ol/source/ImageStatic";
+import {getCenter} from "ol/extent";
 
-const LorePage = () => {
-    const [places, setPlaces] = useState([]);
+const extent = [0, 0, 1024, 968];
+const projection = new Projection({
+    code: 'xkcd-image',
+    units: 'pixels',
+    extent: extent,
+});
 
-    const imageWidth = 1653;
-    const imageHeight = 796;
+const LorePage: React.FC = () => {
+    const mapRef = useRef<HTMLDivElement>(null);
+    const map = useRef<Map | null>(null);
 
-    const clickHandler = (event:any) => {
-        const coordinates = [event.clientX, event.clientY];
-        addPlace(coordinates);
-    }
+    useEffect(() => {
+        if (!mapRef.current) return;
 
-    const addPlace = (position:any) => {
-        // @ts-ignore
-        setPlaces(prevPlaces => [...prevPlaces, <Place position={position}/>]);
-    }
+        const source = new VectorSource();
 
-    return (
-        <ComposableMap width={imageWidth} height={imageHeight} onClick={clickHandler}>
-            <ZoomableGroup minZoom={0.5} maxZoom={10}>
-                <image id={"image"} href={"https://cdn.discordapp.com/attachments/436214161077436426/1215728818174697513/image.png?ex=65fdceb1&is=65eb59b1&hm=b43de10d49dc3ef3bc227ff0c541b10642d10d83accc15ff85e46c3ff016f506&"}/>
-                {places}
-            </ZoomableGroup>
-        </ComposableMap>
-    );
+        map.current = new Map({
+            target: mapRef.current,
+            layers: [
+                new ImageLayer({
+                    source: new Static({
+                        attributions: '© <a href="https://xkcd.com/license.html">xkcd</a>',
+                        url: 'https://imgs.xkcd.com/comics/online_communities.png',
+                        projection: projection,
+                        imageExtent: extent,
+                    })
+                }),
+                new VectorLayer({
+                    source: source,
+                }),
+            ],
+            view: new View({
+                projection: projection,
+                center: getCenter(extent),
+                zoom: 2,
+                maxZoom: 8,
+            }),
+        });
+
+        map.current.on('click', (event) => {
+            const feature = new Feature({
+                geometry: new Point(event.coordinate),
+            });
+
+            feature.setStyle(
+                new Style({
+                    image: new Icon({
+                        src: "https://cdn.discordapp.com/attachments/436214161077436426/1215734565340184636/tempplacelogo.png?ex=65fdd40b&is=65eb5f0b&hm=c12b17b57cf5f6c2fd0f2e64f46daad60b5462efb0b265e3f327c9c6c1d32a2f&",
+                        scale: 1,
+                    }),
+                })
+            );
+
+            source.addFeature(feature);
+        });
+    }, []);
+
+    return <div ref={mapRef} style={{ width: '100%', height: '100vh' }} />;
 };
 
 export default LorePage;
--- src/place/Place.tsx
+++ src/place/Place.tsx
@@ -2,7 +2,8 @@ import React, {useEffect, useState} from "react";
 
 //i don't know why it cant find the import even though it works
 // @ts-ignore
-import {Marker, Point, useZoomPanContext} from "react-simple-maps";
+import {Point} from "ol/geom/Point";
+import {FeatureType} from "ol/format/WFS";
 
 interface PlaceProps {
     link?: string;
@@ -27,17 +28,8 @@ const Place = (props: PlaceProps) => {
         props.link = "https://cdn.discordapp.com/attachments/436214161077436426/1215734565340184636/tempplacelogo.png?ex=65fdd40b&is=65eb5f0b&hm=c12b17b57cf5f6c2fd0f2e64f46daad60b5462efb0b265e3f327c9c6c1d32a2f&"
     }
 
-    const [projectedPosition, setProjectedPosition] = useState([0,0])
-    const context: zoomPanContext = useZoomPanContext();
-    const { x, y, k } = context;
-
-    useEffect(() => {
-        // @ts-ignore
-        setProjectedPosition([(props.position.at(0)-x)/k-(props.size?.at(0))/2, (props.position.at(1)-y)/k-(props.size?.at(1))/2]);
-    }, [])
-
      return (
-            <image x={projectedPosition.at(0)} y = {projectedPosition.at(1)} style={{width: props.size?.at(0), height: props.size?.at(1)}}
+            <image x={props.position.at(0)} y = {props.position.at(1)} style={{width: props.size?.at(0), height: props.size?.at(1)}}
                    href={props.link}/>
     );
 };
