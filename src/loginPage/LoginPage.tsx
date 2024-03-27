b92bb308cc480508b41baaf35f0bb08f86ddab62
Osariusz <osariusz@interia.pl>
2024-03-27 18:14:48 +0100

    Login page

--- src/loginPage/LoginPage.tsx
+++ src/loginPage/LoginPage.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Button, TextField, Container, Typography } from '@mui/material';
 import styled from '@emotion/styled';
+import axios from 'axios';
 
 const StyledContainer = styled(Container)`
   display: flex;
@@ -18,6 +19,34 @@ const StyledForm = styled('form')`
 `;
 
 const LoginPage: React.FC = () => {
+
+    const TEMP_BACKEND = "http://localhost:8080/api/auth/login";
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+
+    const onUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(e.target.value);
+    };
+    const onPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    };
+
+    const submit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(TEMP_BACKEND, {
+                    username,
+                    password
+                }
+            )
+            console.log(response);
+        } catch (error: any) {
+            console.log("Login failed" + error.message);
+        }
+    }
+
     return (
         <StyledContainer>
             <Typography variant="h4" component="h1">
@@ -30,10 +59,11 @@ const LoginPage: React.FC = () => {
                     required
                     fullWidth
                     id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
+                    label="Username"
+                    name="username"
+                    autoComplete="username"
                     autoFocus
+                    onChange={onUsernameChange}
                 />
                 <TextField
                     variant="outlined"
@@ -45,12 +75,14 @@ const LoginPage: React.FC = () => {
                     type="password"
                     id="password"
                     autoComplete="current-password"
+                    onChange={onPasswordChange}
                 />
                 <Button
                     type="submit"
                     fullWidth
                     variant="contained"
                     color="primary"
+                    onClick={submit}
                 >
                     Sign In
                 </Button>
